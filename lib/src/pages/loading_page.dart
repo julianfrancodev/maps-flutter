ba220e04e33f5cddf6fa754d0b5e840926d6fe28
julian <juliandanielfranco@gmail.com>
2020-10-04 18:19:59 -0500

    feat: add geolocator

--- lib/src/pages/loading_page.dart
+++ lib/src/pages/loading_page.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
 import 'package:mapbox_flutter/src/helpers/helpers.dart';
 import 'package:mapbox_flutter/src/pages/gps_access_page.dart';
 import 'package:mapbox_flutter/src/pages/map_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class LoadingPage extends StatefulWidget {
   @override
   _LoadingPageState createState() => _LoadingPageState();
 }
 
-class _LoadingPageState extends State<LoadingPage> {
+class _LoadingPageState extends State<LoadingPage> with WidgetsBindingObserver {
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async{
+    if(state == AppLifecycleState.resumed){
+      if(await Geolocator.isLocationServiceEnabled()){
+        Navigator.pushReplacement(context, navigateFadeIn(context, MapPage()));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
         future: checkGpsLocation(context),
         builder: (context, snapshot){
-          return  Center(
-            child: CircularProgressIndicator(strokeWidth: 2,),
-          );
+        if(snapshot.hasData){
+          return Center(child: Text(snapshot.data),);
+        }
+        return CircularProgressIndicator();
         },
       ),
     );
@@ -25,10 +50,20 @@ class _LoadingPageState extends State<LoadingPage> {
 
   Future checkGpsLocation(BuildContext context) async{
     // TODO permiso gps
+    final gpsPermission = await Permission.location.isGranted;
     // TODO enable gps
-    await Future.delayed(Duration(milliseconds: 100));
-    Navigator.pushReplacement(context, navigateFadeIn(context, GpsAccessPage()));
-    // Navigator.pushReplacement(context, navigateFadeIn(context, MapPage()));
+    final gpsEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(gpsPermission && gpsEnabled){
+      Navigator.pushReplacement(context, navigateFadeIn(context, MapPage()));
+      return '';
+    }else if(!gpsPermission){
+      Navigator.pushReplacement(context, navigateFadeIn(context, GpsAccessPage()));
+      return 'Activa el gps';
+    }else {
+      return 'Necesitas activar el gps';
+    }
+
 
   }
 }

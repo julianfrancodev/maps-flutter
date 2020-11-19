a019258e858a14e85ee5f47b4e3b2ff8d9db6c39
julian <juliandanielfranco@gmail.com>
2020-11-19 18:22:46 -0500

    feat: improve google maps flutter

--- lib/src/bloc/my_location_bloc.dart
+++ lib/src/bloc/my_location_bloc.dart
@@ -1,18 +1,43 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:geolocator/geolocator.dart' as Geolocator;
 import 'package:meta/meta.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart' show LatLng;
+
 part 'my_location_event.dart';
+
 part 'my_location_state.dart';
 
 class MyLocationBloc extends Bloc<MyLocationEvent, MyLocationState> {
   MyLocationBloc() : super(MyLocationState());
 
+  StreamSubscription<Geolocator.Position> _positionSubscription;
+
+  void startFollowing() {
+   this._positionSubscription = Geolocator.getPositionStream(
+            desiredAccuracy: Geolocator.LocationAccuracy.high,
+            distanceFilter: 10)
+        .listen((Geolocator.Position position) {
+      print(position);
+      final newLocation = new LatLng(position.latitude, position.longitude);
+      add(OnLocationChange(newLocation));
+    });
+  }
+
+  void disposeFollowing(){
+    this._positionSubscription?.cancel();
+  }
+
   @override
   Stream<MyLocationState> mapEventToState(
     MyLocationEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if(event is OnLocationChange){
+      yield state.copyWith(
+        validateLocation: true,
+        location: event.location,
+      );
+    }
   }
 }
--- lib/src/pages/map_page.dart
+++ lib/src/pages/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mapbox_flutter/src/bloc/my_location_bloc.dart';
 
 class MapPage extends StatefulWidget {
   @override
@@ -7,11 +9,36 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   @override
+  void initState() {
+    // TODO: implement initState
+
+    context.bloc<MyLocationBloc>().startFollowing();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    context.bloc<MyLocationBloc>().disposeFollowing();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Map Page"),
+      body: BlocBuilder<MyLocationBloc, MyLocationState>(
+        builder: (BuildContext context, state) {
+          return renderMap(state);
+        },
       ),
     );
+  }
+
+  Widget renderMap(MyLocationState myLocationState){
+    if(!myLocationState.validateLocation) return Text('Location...');
+    return Text('state ${myLocationState.location.longitude}');
+
   }
 }

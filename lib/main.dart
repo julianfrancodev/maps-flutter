69185f055b7ce92a487131f840d716ec4606dd58
julian <juliandanielfranco@gmail.com>
2020-12-05 19:54:58 -0500

    feat: improve search bloc

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mapbox_flutter/src/bloc/map_bloc.dart';
 import 'package:mapbox_flutter/src/bloc/my_location_bloc.dart';
+import 'package:mapbox_flutter/src/bloc/search_bloc.dart';
 import 'package:mapbox_flutter/src/routes/routes.dart';
 
 void main(){
@@ -9,7 +10,8 @@ void main(){
     MultiBlocProvider(
       providers: [
         BlocProvider(create: (_)=> MyLocationBloc()),
-        BlocProvider(create: (_)=> MapBloc(),)
+        BlocProvider(create: (_)=> MapBloc(),),
+        BlocProvider(create: (_)=>SearchBloc(),)
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,

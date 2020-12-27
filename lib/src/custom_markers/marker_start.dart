c0783969a95a6478287d911747a63844452cb17b
julian <juliandanielfranco@gmail.com>
2020-12-27 14:13:52 -0500

    feat: improve marker

--- lib/src/custom_markers/marker_start.dart
+++ lib/src/custom_markers/marker_start.dart
@@ -6,7 +6,6 @@ class MarkerStart extends CustomPainter {
     final double circleRadius = 20;
     final double circleRadiusWhite = 20;
 
-
     Paint paint = new Paint()..color = Colors.black87;
 
     // Draw black cricle
@@ -20,7 +19,28 @@ class MarkerStart extends CustomPainter {
 
     canvas.drawCircle(
         Offset(circleRadiusWhite, size.height - circleRadiusWhite), 7, paint);
+    // Shadow
+    final Path path = new Path();
+    path.moveTo(40, 20);
 
+    path.lineTo(size.width - 10, 20);
+
+    path.lineTo(size.width - 10, 100);
+
+    path.lineTo(40, 100);
+
+    canvas.drawShadow(path, Colors.black87, 10, false);
+
+    // draw box
+
+    final whiteBox = Rect.fromLTWH(40, 20, size.width - 55, 80);
+
+    canvas.drawRect(whiteBox, paint);
+
+    paint.color = Colors.black;
+    final blackBox = Rect.fromLTWH(30, 20, 70, 80);
+
+    canvas.drawRect(blackBox, paint);
   }
 
   @override
